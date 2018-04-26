4bd3b3e683bfa46fc3707a795fbe7d2fc4878000
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-04-26 09:55:21 +0200

    first commit

---
+++ Assets/Scripts/PressStart.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressStart : MonoBehaviour {
+
+    public GameObject pressStart;
+    float time;
+
+	
+	void Update ()
+    {
+        time += Time.deltaTime;
+
+        if(Mathf.RoundToInt(time)%2 == 0)
+        {
+            pressStart.SetActive(true);
+        }
+        else
+        {
+            pressStart.SetActive(false);
+        }
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("selectMode");
+    }
+}
