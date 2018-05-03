31d4bbfd3a735b01ecd3e9e95812d8a2d8eaf013
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-05-03 20:52:04 +0200

    some changes

---
+++ Assets/Scripts/Background/FrameController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FrameController : MonoBehaviour {
+
+    float time;
+    Image borderColor;
+
+    bool danger;
+
+    Color normalColor;
+    Color dangerColor;
+
+    private void Awake()
+    {
+        borderColor = GetComponent<Image>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        normalColor = borderColor.color;
+        dangerColor = new Color(240f/255, 0, 40f/255);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        time = GameManager.gm.time;
+
+        if(time<100 && !danger && GameManager.inGame)
+        {
+            StartCoroutine(Danger());
+        }
+     
+
+	}
+
+    IEnumerator Danger()
+    {
+        danger = true;
+
+        while ( time> 0 && GameManager.inGame)
+        {
+            if(borderColor.color == normalColor)
+            {
+                borderColor.color = dangerColor;
+            }
+            else
+            {
+                borderColor.color = normalColor;
+            }
+
+            yield return new WaitForSeconds(0.4f);
+        }
+
+        borderColor.color = normalColor;
+    }
+}
---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour {
+
+    public Transform[] nodes;
+    float step = 1f;
+
+    //with notification center we will know when a world has been completed;
+    private void Awake()
+    {
+        NotificationCenter.DefaultCenter().AddObserver(this,"MoveToNextNode");
+    }
+
+
+    public void MoveToNextNode(Notification notification)
+    {
+        int i = (int)notification.data;
+        transform.position = Vector3.MoveTowards(transform.position, nodes[i].position, step * Time.deltaTime);
+        transform.LookAt(nodes[i]);
+    }
+}
