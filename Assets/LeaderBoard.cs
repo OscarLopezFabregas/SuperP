67ee528de1b7751f54bb6471725c8d91e9ec133e
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-05-15 10:10:58 +0200

    google play services integration

---
+++ Assets/LeaderBoard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+
+public class LeaderBoard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        if(Social.localUser.authenticated)
+        {
+            ((PlayGamesPlatform)Social.Active).ShowLeaderboardUI("CgkItby8-JQLEAIQAQ");
+        }
+        else
+        {
+            Social.localUser.Authenticate((bool success) => { });
+        }
+    }
+}
